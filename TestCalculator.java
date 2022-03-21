3607c8e3e3dfd9682cd997e972d757a6b2522ac7
Sravanthi1108 <101916647+Sravanthi1108@users.noreply.github.com>
2022-03-21 19:19:39 +0530

    file is added

---
+++ TestCalculator.java
@@ -0,0 +1,15 @@
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator=new Calculator();
+		System.out.println(calculator.add(30,40));
+		System.out.println(calculator.sub(32,20));
+		System.out.println(calculator.mult(20,15));
+		System.out.println(calculator.div(2.5f,1.2f));
+		System.out.println(calculator.square(5));
+		System.out.println(calculator.remainder(4,25));
+	}
+
+}
