c36c7d06eef4e0df0a9c6fa2a240685176936c6a
Sravanthi1108 <101916647+Sravanthi1108@users.noreply.github.com>
2022-03-21 19:09:07 +0530

    Arithmatic.java

---
+++ Arithematic.java
@@ -0,0 +1,24 @@
+
+public class Arithematic {
+	public int add(int num1,int num2)
+	{
+		return num1+num2;
+	}
+    public int sub(int num1,int num2)
+    {
+    	return num1-num2;
+    }
+    public int mul(int num1, int num2)
+    {
+    	return num2*num1;
+    }
+    public float div(float num1,float num2)
+    {
+    	return num1/num2;
+    }
+    public int remainder(int num1,int num2)
+    {
+    	return num2%num1;
+    	
+    }
+}
