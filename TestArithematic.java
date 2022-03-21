be454828a8489e9d9508c5774fdf4222acd5176e
Sravanthi1108 <101916647+Sravanthi1108@users.noreply.github.com>
2022-03-21 19:08:23 +0530

    TestArithematic.java

---
+++ TestArithematic.java
@@ -0,0 +1,20 @@
+
+public class TestArithematic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Arithematic arithematic=new Arithematic();
+		System.out.println("the addition of given numbers is: ");
+		System.out.println(arithematic.add(20,30));
+		System.out.println("the subtraction of given numbers is: ");
+		System.out.println(arithematic.sub(35, 10));
+		System.out.println("the multiplication of given numbers is: ");
+		System.out.println(arithematic.mul(98, 10));
+		System.out.println("the division of given numbers is: ");
+		System.out.println(arithematic.div(2.5f, 1.2f));
+		System.out.println("the modulos of given numbers is: ");
+		System.out.println(arithematic.remainder(4, 25));
+
+	}
+
+}
