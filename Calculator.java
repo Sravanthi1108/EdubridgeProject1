a54b3a1dfc326bc1a26153d10efcb78153d13764
Sravanthi1108 <101916647+Sravanthi1108@users.noreply.github.com>
2022-03-21 19:19:15 +0530

    file is added

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+
+public class Calculator {
+	 
+	public int add(int num1,int num2)
+	{
+		return num1+num2;
+	}
+	public int sub(int num1,int num2)
+	{
+		return num1-num2;
+	}
+	public int mult(int num1,int num2)
+	{
+		return num1*num2;
+	}
+	public float div(float num1,float num2)
+	{
+		return num1/num2;
+	}
+	public int square(int num1)
+	{
+		return num1*num1;
+	}
+	public int remainder(int num1,int num2)
+	{
+		return num2%num1;
+	}
+
+}
